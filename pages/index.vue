540b43af671cbbe5da3819d9247f11b91c6b9a78
Leon Harper <leon@ecowebhosting.co.uk>
2018-07-04 18:13:06 +0100

    initital commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<!-- pages/index.vue -->
+
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hi, I'm Leon Harper.</h1>
+      <h2 class="subtitle">A Developer based in the UK..</h2>
+    </div>
+  </section>
+</template>
+
+<section class="hero is-dark">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Hero title
+      </h1>
+      <h2 class="subtitle">
+        Hero subtitle
+      </h2>
+    </div>
+  </div>
+</section>
